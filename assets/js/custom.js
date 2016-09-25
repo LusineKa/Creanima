b6cce68b552ec0da13bf67736d1bf772307a20a7
Lusya <karapetyanlusine1993@gmail.com>
2016-09-25 10:35:54 +0300

    test

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -102,4 +102,17 @@ jQuery(document).ready(function($){
         });
 
     });
+
+    $(function(){
+        if (/iPhone|iPod|iPad/.test(navigator.userAgent))
+            $('.cd-modal').wrap(function(){
+                var $this = $(this);
+                return $('<div />').css({
+                    width: $this.attr('width'),
+                    height: $this.attr('height'),
+                    overflow: 'scroll',
+                    '-webkit-overflow-scrolling': 'touch'
+                });
+            });
+    })
 });
