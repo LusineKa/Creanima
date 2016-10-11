e8054bb3c38383ad1e2b75e8934003565a424e2f
Lusine <karapetyanlusine1993@gmail.com>
2016-10-11 12:29:40 +0300

    services content

--- services.php
+++ services.php
@@ -3,7 +3,7 @@
     <div class="inner-banner"
          style=" background:url(img/banner-bg.jpg) no-repeat top center fixed; background-size:cover;">
         <div class="overlay-mask">
-            <div class="container"><h2>Services</h2></div>
+            <div class="container"><h2>УСЛУГИ</h2></div>
         </div>
     </div>
 </section>
@@ -13,7 +13,7 @@
             <div class="container">
                 <div class="service-area-heading">
                     <div class="col-md-12">
-                        <div class="main-title"><h3> Our Services </h3></div>
+                        <div class="main-title"><h3> НАШИ УСЛУГИ </h3></div>
                     </div>
                 </div>
             </div>
@@ -22,47 +22,47 @@
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-1.png"></div>
-                            <div class="features-text"><h5>Logo Design</h5>
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                    laudantium, totam rem aperiam.</p></div>
+                            <div class="features-text"><h5>Заказать Логотип</h5>
+                                <p>МЫ РАБОТАЕМ С МАЛЫМ И СРЕДНИМ БИЗНЕСОМ,
+                                    потребности наших клиентов делятся на две соответствующие категории, поэтому мы разработали два разных подхода.</p></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-2.png"></div>
-                            <div class="features-text"><h5>Graphic Design</h5>
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                    laudantium, totam rem aperiam.</p></div>
+                            <div class="features-text"><h5>Графический дизайн</h5>
+                                <p>Немалый опыт работы в рекламной среде, использование новейших
+                                    программных разработок позволяют решение вопроса, создавая уникальный дизайн продукции для любого заказа.</p></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-3.png"></div>
-                            <div class="features-text"><h5>Ilustrations</h5>
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                    laudantium, totam rem aperiam.</p></div>
+                            <div class="features-text"><h5>Заказать иллюстрацию</h5>
+                                <p>Заказать иллюстрацию здесь даже проще, чем купить наушники в интернет-магазине.
+                                    Адекватные цены, скорость исполнения, а главное, 100% гарантия возврата средств!</p></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-4.png"></div>
-                            <div class="features-text"><h5>Responsive Design</h5>
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                    laudantium, totam rem aperiam.</p></div>
+                            <div class="features-text"><h5>Адаптивный дизайн</h5>
+                                <p>Заказать дизайн для сайта у профессионалов —
+                                    первый шаг к повышению у продаж .</p></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-5.png"></div>
-                            <div class="features-text"><h5>SEO</h5>
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                                    laudantium, totam rem aperiam.</p></div>
+                            <div class="features-text"><h5>SEO продвижение</h5>
+                                <p>Если, на следующий месяц, после истечения срока продвижения,
+                                    в ТОП-10 вышло менее 50% фраз.литературная площадка русского мира.</p></div>
                         </div>
                     </div>
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-6.png"></div>
-                            <div class="features-text"><h5>Photography</h5>
+                            <div class="features-text"><h5>Создание фото сайта</h5>
                                 <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
                                     laudantium, totam rem aperiam.</p></div>
                         </div>
@@ -70,7 +70,7 @@
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-7.png"></div>
-                            <div class="features-text"><h5>Email Marketing</h5>
+                            <div class="features-text"><h5>Email-маркетинг</h5>
                                 <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
                                     laudantium, totam rem aperiam.</p></div>
                         </div>
@@ -78,7 +78,7 @@
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-8.png"></div>
-                            <div class="features-text"><h5>Server Support</h5>
+                            <div class="features-text"><h5>Продвижение сайтов</h5>
                                 <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
                                     laudantium, totam rem aperiam.</p></div>
                         </div>
@@ -86,7 +86,7 @@
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-9.png"></div>
-                            <div class="features-text"><h5>eCommerce</h5>
+                            <div class="features-text"><h5>Бизнес-сайт</h5>
                                 <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
                                     laudantium, totam rem aperiam.</p></div>
                         </div>
@@ -94,7 +94,7 @@
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-10.png"></div>
-                            <div class="features-text"><h5>Mobile Apps</h5>
+                            <div class="features-text"><h5>Мобильные приложения </h5>
                                 <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
                                     laudantium, totam rem aperiam.</p></div>
                         </div>
@@ -102,7 +102,7 @@
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-11.png"></div>
-                            <div class="features-text"><h5>Content Writing</h5>
+                            <div class="features-text"><h5>Контент сайта</h5>
                                 <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
                                     laudantium, totam rem aperiam.</p></div>
                         </div>
@@ -110,7 +110,7 @@
                     <div class="col-md-4">
                         <div class="features-box">
                             <div class="features-icon"><img alt="" src="img/feature-icon-12.png"></div>
-                            <div class="features-text"><h5>Blogs</h5>
+                            <div class="features-text"><h5>Блог</h5>
                                 <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
                                     laudantium, totam rem aperiam.</p></div>
                         </div>
