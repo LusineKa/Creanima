0d0735dbd5e2d1f3dbc9d023f9ce9d363aa9ce8e
Ararat <tonoyandeveloper@gmail.com>
2016-10-30 19:16:58 +0300

    Cap page edit

--- about.php
+++ about.php
@@ -1,37 +1,44 @@
 <?php include 'header.php'; ?>
 
-<section class="banner-sec">
-    <div class="inner-banner"
-         style=" background:url(img/banner-bg.jpg) no-repeat top center fixed; background-size:cover;">
-        <div class="overlay-mask"></div>
-    </div>
-</section>
-<section class="content-area-sec">
-    <div class="content-area-with-bg no-team">
-        <div class="about-area-sec">
-            <div class="container">
-                <div class="about-us-area">
-                    <div class="col-md-12">
-                        <div class="main-title about"><h3>
-                                Создать сайт в Компании «Creanima»</h3></div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="image-holder"><img src="img/about-item.png" alt=""></div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="about-text-sec"><h5>СОЗДАНИЕ САЙТОВ – НАШЕ ПРИЗВАНИЕ!</h5>
-                            <p><b class="big-ben"> Студио Creanima</b> основана в 2011 году. Мы опытные и компетентные. Создание сайтов – основное направление нашей
-                                       деятельности. У нас 5 лет опыта, более <b class="big-ben">400 проектов</b>. Больше половины клиентов обращаются к нам повторно или приходят по рекомендациям.
-                            </p>
-                            <p><b class="big-ben">Мы — команда</b> молодых и амбициозных людей, которых объединяет страсть к любимому делу.
-                                В команде Creanima 6 человек - талантливый дизайнер, профессиональный front-end верстальщик, универсальный программист, SEO-специалист, бизнес аналитик, арт-креативщик, тестеровщик и проект менеджер.
-                                Инженерный состав нашей компании – это эксперты в различных отраслях ИТ.</p>
+    <section class="banner-sec">
+        <div class="inner-banner"
+             style=" background:url(img/banner-bg.jpg) no-repeat top center fixed; background-size:cover;">
+            <div class="overlay-mask"></div>
+        </div>
+    </section>
+    <section class="content-area-sec">
+        <div class="content-area-with-bg no-team">
+            <div class="about-area-sec">
+                <div class="container">
+                    <div class="about-us-area">
+                        <div class="col-md-12">
+                            <div class="main-title about"><h3>
+                                    Создать сайт в Компании «Creanima»</h3></div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="image-holder"><img src="img/about-item.png" alt=""></div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="about-text-sec"><h5>СОЗДАНИЕ САЙТОВ – НАШЕ ПРИЗВАНИЕ!</h5>
+                                <p><b class="big-ben"> Студио Creanima</b> основана в 2012 году. Мы опытные и
+                                    компетентные. Создание сайтов – основное направление нашей
+                                    деятельности. У нас 4 лет опыта, более <b class="big-ben">200 проектов</b>. Больше
+                                    половины клиентов обращаются к нам повторно или приходят по рекомендациям.
+                                </p>
+                                <p><b class="big-ben">Мы — команда</b> молодых и амбициозных людей, которых объединяет
+                                    страсть к любимому делу.
+                                    В команде Creanima 8 человек - талантливый дизайнер, профессиональный front-end
+                                    верстальщик, универсальный программист, SEO-специалист, бизнес аналитик,
+                                    арт-креативщик, тестеровщик и проект менеджер.</p>
+                                <p>У нас есть <b class="big-ben">совесть</b> и <b class="big-ben">ответственность</b>.
+                                    Для нас главное <b class="big-ben">хорошое имя</b> и <b class="big-ben">довольные
+                                        клиенты</b></p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
 <?php include 'footer.php'; ?>
